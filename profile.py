0cadf48b401e67934305785050caef1a18245142
krishaBhattarai <np05cp4a240020@iic.edu.np>
2026-08-06 12:30:04 +0545

    Fix browser compatibility for high score

--- profile.py
+++ profile.py
@@ -1,59 +1,50 @@
+import sys
+
 class Profile:
 
     def __init__(self, name):
-
         self.name = name
         self.score = 0
-        self.best_score = self.load_high_score()
 
-
+        # Browser can't use highscore.txt
+        if sys.platform == "emscripten":
+            self.best_score = 0
+        else:
+            self.best_score = self.load_high_score()
 
     def increase_score(self):
 
         self.score += 1
 
-
         if self.score > self.best_score:
 
             self.best_score = self.score
 
-            self.save_high_score()
-
-
+            if sys.platform != "emscripten":
+                self.save_high_score()
 
     def reset_score(self):
-
         self.score = 0
-
-
 
     def save_high_score(self):
 
-        with open("highscore.txt", "w") as file:
-
-            file.write(
-                f"{self.name},{self.best_score}"
-            )
-
-
+        try:
+            with open("highscore.txt", "w") as file:
+                file.write(f"{self.name},{self.best_score}")
+        except:
+            pass
 
     def load_high_score(self):
 
         try:
-
             with open("highscore.txt", "r") as file:
 
                 data = file.read().split(",")
 
-
-                if data[0] == self.name:
-
+                if len(data) == 2 and data[0] == self.name:
                     return int(data[1])
 
-
         except:
-
-            return 0
-
+            pass
 
         return 0
